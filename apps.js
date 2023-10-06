f1cdd1385ea7272c8c354d1b70b6acaff0f8cb62
Leillacarmene <nti.leillacarmene@gmail.com>
2023-10-06 13:49:35 +0200

    first commit

---
+++ apps.js
@@ -0,0 +1,33 @@
+/*for (let i = 1; i <= 10; i++) {
+    console.log(i); //here is to increase from 1 to 10 in for loop()
+}
+for (let i = 10; i >= 1; i--) {
+    console.log(i);//here is to decrease from 10 to 1 in for loop()
+}
+
+/*for (let i = 10; i >= 1; i--) {
+    console.log(i + "_");//here is to concatinate a number with -
+}
+
+ 
+const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+  ]);
+  console.log(fruits);
+  console.log(fruits.get("bananas"));*/
+
+
+  // Create a Map
+const fruits = new Map();
+
+// Set Map Values
+fruits.set("apples", 500);
+fruits.set("bananas", 300);
+fruits.set("oranges", 200);
+console.log(fruits.get("oranges"));
+
+
+
+ 
